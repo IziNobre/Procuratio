0e3fa1ec2c12ce0e1f9a82396aad3f40f11353bc
Marianka Starke Doring Caresia <112444551+mariankasdc@users.noreply.github.com>
2022-10-27 09:25:15 -0300

    Criada a Classe Endereco

---
+++ src/main/java/br/senac/procuratio/modelo/entidade/endereco/Endereco.java
@@ -0,0 +1,84 @@
+package br.senac.procuratio.modelo.entidade.endereco;
+
+public class Endereco {
+	
+	private String logradouro;
+	private short numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String cep;
+	private String uf;
+	
+	public Endereco (String logradouro, short numero, String complemento, String bairro, String cidade, String cep, String uf) {
+		
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.cep = cep;
+		this.uf = uf;		
+				
+	}
+	
+	public String getLogradouro () {
+		return logradouro;
+	}
+	public void setLogradouro (String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public short getNumero() {
+		return numero;
+	}
+	public void setNumero (short numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento (String complemento) {
+		this.complemento = complemento;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro (String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade (String cidade) {
+		this.cidade = cidade;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep (String cep) {
+		this.cep = cep;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf (String uf) {
+		this.uf = uf;
+	}
+	public boolean equals (Object objeto) {
+		
+		if (objeto == null)
+			return false;
+		
+		if (this == objeto)
+			return true;
+		
+		if (this.getClass() != objeto.getClass())
+			return false;
+		
+		Endereco endereco = (Endereco) objeto;
+		
+		return this.getLogradouro().equals(endereco.getLogradouro()) && this.getNumero() == endereco.getNumero() && this.getComplemento().equals (endereco.getComplemento()) && this.getBairro().equals (endereco.getBairro()) && this.getCep().equals (endereco.getCep()) && this.getCidade().equals (endereco.getCidade()) && this.getUf().equals (endereco.getUf()); 
+				
+	}
+	
+}
